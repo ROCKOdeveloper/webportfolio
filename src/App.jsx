adaa45d2014b71f9b0bb18d6d5a356e22850f46f
Rdeveloper <rockoraboloco@gmail.com>
2024-08-16 15:16:34 -0500

    segundo intento
    quiero ver como funciona esta animación en internet y dispositivos mobiles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { IconMoon, IconSun } from "./components/Icons";
 import "./index.css";
 
 export default function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
   const ref = useRef(null);
 
   const toggleDarkMode = async (isDarkMode) => {
@@ -24,7 +24,6 @@ export default function App() {
       });
     }).ready;
 
-    // Obtener la posición del elemento que activa el cambio
     const { top, left, width, height } = ref.current.getBoundingClientRect();
     const x = left + width / 2;
     const y = top + height / 2;
@@ -32,7 +31,6 @@ export default function App() {
     const bottom = window.innerHeight - top;
     const maxRadius = Math.hypot(Math.max(left, right), Math.max(top, bottom));
 
-    // Aplicar la animación usando View Transitions API
     document.documentElement.animate(
       {
         clipPath: [
@@ -57,12 +55,22 @@ export default function App() {
   }, [isDarkMode]);
 
   return (
-    <div className={`h-screen w-screen flex items-center justify-center ${isDarkMode ? 'bg-gray-950 text-white' : 'bg-gray-100 text-black'}`}>
-      <Switch.Root checked={isDarkMode} onCheckedChange={toggleDarkMode}>
-        <Switch.Thumb ref={ref}>
-          {isDarkMode ? <IconMoon /> : <IconSun />}
-        </Switch.Thumb>
-      </Switch.Root>
-    </div>
+    <>
+      <div
+        className={`h-screen w-screen flex items-center justify-center ${
+          isDarkMode ? "bg-gray-950 text-white" : "bg-gray-100 text-black"
+        }`}
+      >
+        <Switch.Root
+          checked={isDarkMode}
+          onCheckedChange={toggleDarkMode}
+          isDarkMode={isDarkMode}
+        >
+          <Switch.Thumb ref={ref} isDarkMode={isDarkMode}>
+            {isDarkMode ? <IconMoon /> : <IconSun />}
+          </Switch.Thumb>
+        </Switch.Root>
+      </div>
+    </>
   );
 }
--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -2,11 +2,11 @@ import { forwardRef } from "react";
 import * as Switch from "@radix-ui/react-switch";
 import PropTypes from "prop-types";
 
-export const Root = forwardRef(function Root(props, ref) {
+export const Root = forwardRef(function Root({ isDarkMode, className, ...props }, ref) {
   return (
     <Switch.Root
-      className={`w-[40px] h-[22px] rounded-full bg-gray-200 ring-1 ring-gray-300 dark:bg-gray-800 dark:ring-gray-700 outline-none transition-colors ${
-        props.className ?? ""
+      className={`w-[40px] h-[22px] rounded-full ${isDarkMode ? 'bg-gray-700 ring-gray-700' : 'bg-gray-200 ring-gray-300'} outline-none transition-colors ${
+        className ?? ""
       }`}
       {...props}
       ref={ref}
@@ -16,13 +16,14 @@ export const Root = forwardRef(function Root(props, ref) {
 
 Root.propTypes = {
   className: PropTypes.string,
+  isDarkMode: PropTypes.bool.isRequired,
 };
 
-export const Thumb = forwardRef(function Thumb(props, ref) {
+export const Thumb = forwardRef(function Thumb({ isDarkMode, className, ...props }, ref) {
   return (
     <Switch.Thumb
-      className={`flex items-center justify-center w-[20px] h-[20px] bg-white dark:bg-gray-950 text-gray-600 dark:text-gray-300 shadow rounded-full transition-transform translate-x-px data-[state=checked]:translate-x-[19px] ${
-        props.className ?? ""
+      className={`flex items-center justify-center w-[20px] h-[20px] ${isDarkMode ? 'bg-gray-950 text-gray-300' : 'bg-white text-gray-600'} shadow rounded-full transition-transform translate-x-px data-[state=checked]:translate-x-[19px] ${
+        className ?? ""
       }`}
       {...props}
       ref={ref}
@@ -32,4 +33,5 @@ export const Thumb = forwardRef(function Thumb(props, ref) {
 
 Thumb.propTypes = {
   className: PropTypes.string,
+  isDarkMode: PropTypes.bool.isRequired,
 };
